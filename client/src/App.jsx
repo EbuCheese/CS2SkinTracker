7084d2005cbd22af9240832d975a993891a89a78
ebucheese <ethandonate@gmail.com>
2025-06-13 13:25:29 -0700

    init Vite/React project structure

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div className="text-3xl font-bold text-center mt-10 text-green-400">
+      CS Skin Tracker 📈
+    </div>
+  );
+}
+
+
+export default App
